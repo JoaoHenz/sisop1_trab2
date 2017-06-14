ffcaae39b30e8b3ba3f00c01fa473f2bb9253edd
SaintBallista <jmaniquehenz@gmail.com>
2017-06-14 13:57:30 -0300

    Updated t2fs.c

--- t2fs/src/t2fs.c
+++ t2fs/src/t2fs.c
@@ -7,72 +7,90 @@
 
 
 int identify2 (char *name, int size){
+	char *name_aux = "NicolasPessutto242284 JoaoHenz242251";
+	strcopy(name,name_aux);
 
+	return 0;
 }
 
 
-FILE2 create2 (char *filename){
+FILE2 create2 (char *filename){  // estou bastante confuso 
+	int blocolivre=0;
+	int teste;
+	int endbloco = searchBitmap2(blocolivre);
+	
 
+		if(endbloco=<0)
+			return -1;
+		else{
+
+			teste =write_sector(endbloco, filename);
+			if(teste ==0)
+				return 0; 
+			else
+				return -1;
+
+		}
 }
 
-int delete2 (char *filename){
+int delete2 (char *filename){ //TODO
 
-
-
-}
-
-FILE2 open2 (char *filename){
-
-
-}
-
-
-int close2 (FILE2 handle){
-
-
-}
-
-
-int read2 (FILE2 handle, char *buffer, int size){
-
-}
-
-int write2 (FILE2 handle, char *buffer, int size){
 
 
 }
 
-int truncate2 (FILE2 handle){
-
-
-}
-
-int seek2 (FILE2 handle, DWORD offset){
-
-
-}
-
-int mkdir2 (char *pathname){
+FILE2 open2 (char *filename){//TODO
 
 
 }
 
 
-int rmdir2 (char *pathname){
-
-}
-
-DIR2 opendir2 (char *pathname){
-
-}
-
-
-int readdir2 (DIR2 handle, DIRENT2 *dentry){
+int close2 (FILE2 handle){//TODO
 
 
 }
 
 
-int closedir2 (DIR2 handle){
+int read2 (FILE2 handle, char *buffer, int size){//TODO
+
+}
+
+int write2 (FILE2 handle, char *buffer, int size){//TODO
+
+
+}
+
+int truncate2 (FILE2 handle){//TODO
+
+
+}
+
+int seek2 (FILE2 handle, DWORD offset){//TODO
+
+
+}
+
+int mkdir2 (char *pathname){//TODO
+
+
+}
+
+
+int rmdir2 (char *pathname){//TODO
+
+}
+
+DIR2 opendir2 (char *pathname){//TODO
+
+}
+
+
+int readdir2 (DIR2 handle, DIRENT2 *dentry){//TODO
+
+
+}
+
+
+int closedir2 (DIR2 handle){//TODO
 
 }
